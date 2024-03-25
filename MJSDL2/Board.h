f7d38cb79d62526342d99a34721d197d7e75d8d1
jean-claude-segura <candidatspam001@laposte.net>
2024-03-25 21:58:22 +0100

    Cleaning.

--- MJSDL2/Board.h
+++ MJSDL2/Board.h
@@ -9,15 +9,6 @@ class Board
 public:
 	Board();
 	void FillBoard();
-	int dominos[42] = {
-		4,4,4,4,4,4,4,4,4, // Cercles
-		4,4,4,4,4,4,4,4,4, // Bambous
-		4,4,4,4,4,4,4,4,4, // Caractères
-		1,1,1,1, // Saisons
-		1,1,1,1, // Fleurs
-		4,4,4,4, // Honeurs
-		4,4,4 // Dragons
-	};
 	const std::array<std::tuple<int, int, int, int>, 140>& getBoard();
 	const std::array <int, 4>& getSpeciaux();
 	
