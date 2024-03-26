2171e0ac72265d1a2a21949ff0116aa16b419b3a
jean-claude-segura <candidatspam001@laposte.net>
2024-03-26 02:27:09 +0100

    Xoring SDL helper : comments and cleaning.

--- MJSDL2/SDL_Tools.cpp
+++ MJSDL2/SDL_Tools.cpp
@@ -6,33 +6,35 @@ void SDL_UpperBlitInverted(SDL_Surface* src, SDL_Surface* dest, SDL_Rect& coordo
 	SDL_UpperBlit(src, 0, invert, 0);
 	if(SDL_MUSTLOCK(invert))
 		SDL_LockSurface(invert);
-	//std::cout << "Lock" << std::endl;
+
 	Uint32* bufferZ = (Uint32*)invert->pixels;
 	auto Amask = src->format->Amask;
 	auto RGBmask = 0xFFFFFFFF & ~Amask;
+
 	if (src->format->BitsPerPixel == 32)
 	{
 		for (int pixel = 0; pixel < invert->h * invert->w; ++pixel, ++bufferZ)
 		{
-			/*Uint32 red = src->format->Rmask ^ *bufferZ & src->format->Rmask;
+			/*
+			Uint32 red = src->format->Rmask ^ *bufferZ & src->format->Rmask;
 			Uint32 green = src->format->Gmask ^ *bufferZ & src->format->Gmask;
-			Uint32 blue = src->format->Bmask ^ *bufferZ & src->format->Bmask;*/
+			Uint32 blue = src->format->Bmask ^ *bufferZ & src->format->Bmask;
+			*bufferZ = red | green | blue | alpha;
+			*/
 			
-			/*Uint32 alpha = *bufferZ & Amask;
-
-			Uint32 rgb = RGBmask ^ *bufferZ & RGBmask;*/
-
-			//*bufferZ = red | green | blue | alpha;
-
-			//*bufferZ = rgb | alpha;
-
+			/*
+			Uint32 alpha = *bufferZ & Amask;
+			Uint32 rgb = RGBmask ^ *bufferZ & RGBmask;
+			*bufferZ = rgb | alpha;
+			*/
+			
 			*bufferZ = RGBmask ^ *bufferZ & RGBmask | *bufferZ & Amask;
 		}
 	}
-	//std::cout << "Xored" << std::endl;
+
 	if (SDL_MUSTLOCK(invert))
 		SDL_UnlockSurface(invert);
-	//std::cout << "Unlock" << std::endl;
+
 	SDL_UpperBlit(invert, NULL, dest, &coordonnees);
 	SDL_FreeSurface(invert);
 }
