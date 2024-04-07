2e5340bcf46036912637471f2b338906e461dca0
jean-claude-segura <candidatspam001@laposte.net>
2024-04-07 19:20:16 +0200

    Console supposed to hide on loading...

--- MJSDL2/MJSDL2.cpp
+++ MJSDL2/MJSDL2.cpp
@@ -1,16 +1,23 @@
 #include "GraphicBoard.h"
-
+#ifdef _RELEASE
+#include <windows.h>
+#endif
 int main()
 {
 	try
 	{
+#ifdef _RELEASE
+		ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif
 		GraphicBoard plateau;
 		plateau.Loop();
 	}
 	catch (...)
 	{
-
 	}
 
+#ifdef _RELEASE
+	ShowWindow(GetConsoleWindow(), SW_SHOW);
+#endif
 	return 0;
 }
